4da612cc575cbcb570db4f173b3febc074d0ef34
Mohamed-Gamal-22 <mogamall086@gmail.com>
2025-04-13 21:26:02 +0200

    testimonial slider

--- src/app/_components/testimonials/Testimonials.tsx
+++ src/app/_components/testimonials/Testimonials.tsx
@@ -1,3 +1,4 @@
+"use client"
 import React from 'react'
 import style from "./testimonials.module.css"
 import Image from 'next/image'
@@ -5,106 +6,123 @@ import img1 from "../../../public/assets/images/Group 6.png"
 import { Star } from 'lucide-react'
 import message from "../../../public/assets/images/Group.png"
 
+import Slider from "react-slick";
+import SampleNextArrow from '../Arrows/next-arrow/NextArrow'
+import SamplePrevArrow from '../Arrows/prev-arrow/PrevArrow'
+
+var settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    nextArrow: <SampleNextArrow />,
+    prevArrow: <SamplePrevArrow />
+  };
+
 export default function Testimonials() {
   return <>
     <div className={`${style.testimonials} my-20 py-10  flex flex-wrap`}>
-          <div className="container w-[80%] mx-auto flex">
-          <div className="w-1/4 p-2">
-                <div className="content bg-white p-9 rounded-[100px] rounded-tl-[50px]">
-                    <div className="header flex gap-4">
-                        <div className="left">
-                            <Image src={img1} alt="img1"/>
+          <div className="container w-[80%] mx-auto">
+            <Slider {...settings}>
+                <div className="p-2">
+                    <div className="content bg-white p-9 rounded-[100px] rounded-tl-[50px]">
+                        <div className="header flex gap-4">
+                            <div className="left">
+                                <Image src={img1} alt="img1"/>
+                            </div>
+                            <div className="right font-bold">
+                                <p>Mohamed Gamal</p>
+                                <p className='color-rose'>Customer</p>
+                            </div>
                         </div>
-                        <div className="right font-bold">
-                            <p>Mohamed Gamal</p>
-                            <p className='color-rose'>Customer</p>
+                        <hr className='my-4'/>
+                        <p className='text-sm text-[#757F95]'>Ab vel consequatur repellat eos omnis accusamus porro sunt dolorem. Totam voluptas ullam ut. Neque accusantium voluptas rerum. Dolorem veritatis quo omnis nihil nulla harum eum. Dignissimos laborum necessitatibus vero nihil.</p>
+                        <div className='flex justify-between'>
+                            <div className="footer my-4 flex gap-1">
+                                <Star className="color-rose fill-[#F82BA9]" />
+                                <Star className="color-rose fill-[#F82BA9]" />
+                                <Star className="color-rose fill-[#F82BA9]" />
+                                <Star className="color-rose fill-[#F82BA9]" />
+                            </div>
+                            <Image src={message} alt="message" />
                         </div>
-                    </div>
-                    <hr className='my-4'/>
-                    <p className='text-sm text-[#757F95]'>Ab vel consequatur repellat eos omnis accusamus porro sunt dolorem. Totam voluptas ullam ut. Neque accusantium voluptas rerum. Dolorem veritatis quo omnis nihil nulla harum eum. Dignissimos laborum necessitatibus vero nihil.</p>
-                    <div className='flex justify-between'>
-                        <div className="footer my-4 flex gap-1">
-                            <Star className="color-rose fill-[#F82BA9]" />
-                            <Star className="color-rose fill-[#F82BA9]" />
-                            <Star className="color-rose fill-[#F82BA9]" />
-                            <Star className="color-rose fill-[#F82BA9]" />
-                        </div>
-                        <Image src={message} alt="message" />
                     </div>
                 </div>
-            </div>
-            <div className="w-1/4 p-2">
-                <div className="content bg-white p-9 rounded-[100px] rounded-tl-[50px]">
-                    <div className="header flex gap-4">
-                        <div className="left">
-                            <Image src={img1} alt="img1"/>
+                <div className="p-2">
+                    <div className="content bg-white p-9 rounded-[100px] rounded-tl-[50px]">
+                        <div className="header flex gap-4">
+                            <div className="left">
+                                <Image src={img1} alt="img1"/>
+                            </div>
+                            <div className="right font-bold">
+                                <p>Mohamed Gamal</p>
+                                <p className='color-rose'>Customer</p>
+                            </div>
                         </div>
-                        <div className="right font-bold">
-                            <p>Mohamed Gamal</p>
-                            <p className='color-rose'>Customer</p>
+                        <hr className='my-4'/>
+                        <p className='text-sm text-[#757F95]'>Ab vel consequatur repellat eos omnis accusamus porro sunt dolorem. Totam voluptas ullam ut. Neque accusantium voluptas rerum. Dolorem veritatis quo omnis nihil nulla harum eum. Dignissimos laborum necessitatibus vero nihil.</p>
+                        <div className='flex justify-between'>
+                            <div className="footer my-4 flex gap-1">
+                                <Star className="color-rose fill-[#F82BA9]" />
+                                <Star className="color-rose fill-[#F82BA9]" />
+                                <Star className="color-rose fill-[#F82BA9]" />
+                                <Star className="color-rose fill-[#F82BA9]" />
+                            </div>
+                            <Image src={message} alt="message" />
                         </div>
-                    </div>
-                    <hr className='my-4'/>
-                    <p className='text-sm text-[#757F95]'>Ab vel consequatur repellat eos omnis accusamus porro sunt dolorem. Totam voluptas ullam ut. Neque accusantium voluptas rerum. Dolorem veritatis quo omnis nihil nulla harum eum. Dignissimos laborum necessitatibus vero nihil.</p>
-                    <div className='flex justify-between'>
-                        <div className="footer my-4 flex gap-1">
-                            <Star className="color-rose fill-[#F82BA9]" />
-                            <Star className="color-rose fill-[#F82BA9]" />
-                            <Star className="color-rose fill-[#F82BA9]" />
-                            <Star className="color-rose fill-[#F82BA9]" />
-                        </div>
-                        <Image src={message} alt="message" />
                     </div>
                 </div>
-            </div>
-            <div className="w-1/4 p-2">
-                <div className="content bg-white p-9 rounded-[100px] rounded-tl-[50px]">
-                    <div className="header flex gap-4">
-                        <div className="left">
-                            <Image src={img1} alt="img1"/>
+                <div className="p-2">
+                    <div className="content bg-white p-9 rounded-[100px] rounded-tl-[50px]">
+                        <div className="header flex gap-4">
+                            <div className="left">
+                                <Image src={img1} alt="img1"/>
+                            </div>
+                            <div className="right font-bold">
+                                <p>Mohamed Gamal</p>
+                                <p className='color-rose'>Customer</p>
+                            </div>
                         </div>
-                        <div className="right font-bold">
-                            <p>Mohamed Gamal</p>
-                            <p className='color-rose'>Customer</p>
+                        <hr className='my-4'/>
+                        <p className='text-sm text-[#757F95]'>Ab vel consequatur repellat eos omnis accusamus porro sunt dolorem. Totam voluptas ullam ut. Neque accusantium voluptas rerum. Dolorem veritatis quo omnis nihil nulla harum eum. Dignissimos laborum necessitatibus vero nihil.</p>
+                        <div className='flex justify-between'>
+                            <div className="footer my-4 flex gap-1">
+                                <Star className="color-rose fill-[#F82BA9]" />
+                                <Star className="color-rose fill-[#F82BA9]" />
+                                <Star className="color-rose fill-[#F82BA9]" />
+                                <Star className="color-rose fill-[#F82BA9]" />
+                            </div>
+                            <Image src={message} alt="message" />
                         </div>
-                    </div>
-                    <hr className='my-4'/>
-                    <p className='text-sm text-[#757F95]'>Ab vel consequatur repellat eos omnis accusamus porro sunt dolorem. Totam voluptas ullam ut. Neque accusantium voluptas rerum. Dolorem veritatis quo omnis nihil nulla harum eum. Dignissimos laborum necessitatibus vero nihil.</p>
-                    <div className='flex justify-between'>
-                        <div className="footer my-4 flex gap-1">
-                            <Star className="color-rose fill-[#F82BA9]" />
-                            <Star className="color-rose fill-[#F82BA9]" />
-                            <Star className="color-rose fill-[#F82BA9]" />
-                            <Star className="color-rose fill-[#F82BA9]" />
-                        </div>
-                        <Image src={message} alt="message" />
                     </div>
                 </div>
-            </div>
-            <div className="w-1/4 p-2">
-                <div className="content bg-white p-9 rounded-[100px] rounded-tl-[50px]">
-                    <div className="header flex gap-4">
-                        <div className="left">
-                            <Image src={img1} alt="img1"/>
+                <div className="p-2">
+                    <div className="content bg-white p-9 rounded-[100px] rounded-tl-[50px]">
+                        <div className="header flex gap-4">
+                            <div className="left">
+                                <Image src={img1} alt="img1"/>
+                            </div>
+                            <div className="right font-bold">
+                                <p>Mohamed Gamal</p>
+                                <p className='color-rose'>Customer</p>
+                            </div>
                         </div>
-                        <div className="right font-bold">
-                            <p>Mohamed Gamal</p>
-                            <p className='color-rose'>Customer</p>
+                        <hr className='my-4'/>
+                        <p className='text-sm text-[#757F95]'>Ab vel consequatur repellat eos omnis accusamus porro sunt dolorem. Totam voluptas ullam ut. Neque accusantium voluptas rerum. Dolorem veritatis quo omnis nihil nulla harum eum. Dignissimos laborum necessitatibus vero nihil.</p>
+                        <div className='flex justify-between'>
+                            <div className="footer my-4 flex gap-1">
+                                <Star className="color-rose fill-[#F82BA9]" />
+                                <Star className="color-rose fill-[#F82BA9]" />
+                                <Star className="color-rose fill-[#F82BA9]" />
+                                <Star className="color-rose fill-[#F82BA9]" />
+                            </div>
+                            <Image src={message} alt="message" />
                         </div>
-                    </div>
-                    <hr className='my-4'/>
-                    <p className='text-sm text-[#757F95]'>Ab vel consequatur repellat eos omnis accusamus porro sunt dolorem. Totam voluptas ullam ut. Neque accusantium voluptas rerum. Dolorem veritatis quo omnis nihil nulla harum eum. Dignissimos laborum necessitatibus vero nihil.</p>
-                    <div className='flex justify-between'>
-                        <div className="footer my-4 flex gap-1">
-                            <Star className="color-rose fill-[#F82BA9]" />
-                            <Star className="color-rose fill-[#F82BA9]" />
-                            <Star className="color-rose fill-[#F82BA9]" />
-                            <Star className="color-rose fill-[#F82BA9]" />
-                        </div>
-                        <Image src={message} alt="message" />
                     </div>
                 </div>
-            </div>
+            </Slider>
+            
           </div>
     </div>
   </>
